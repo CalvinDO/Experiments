957ec5017b0c5f0a43a6d4c5f456376bcea38ce8
CalvinDO <calvindelloro@mail.de>
2019-09-26 21:35:12 +0200

    set up vector in wingsuit

--- Wingsuit/Main.js
+++ Wingsuit/Main.js
@@ -1,6 +1,7 @@
 var Wingsuit;
 (function (Wingsuit) {
     let crc2;
+    var Vector2D = Vector.Vector2D;
     window.addEventListener("load", init);
     function init(_event) {
         // wenn mit Canvas gearbeitet werden soll
@@ -8,6 +9,8 @@ var Wingsuit;
         crc2 = canvas.getContext("2d");
         // wenn der Ursprung des Koordinatensystems in der Mitte liegen soll
         crc2.translate(canvas.width / 2, canvas.height / 2);
+        let vector = new Vector2D(0, 0);
+        console.log(vector);
     }
 })(Wingsuit || (Wingsuit = {}));
 //# sourceMappingURL=Main.js.map
--- Wingsuit/Main.ts
+++ Wingsuit/Main.ts
@@ -1,6 +1,8 @@
 namespace Wingsuit {
     let crc2: CanvasRenderingContext2D;
 
+    import Vector2D = Vector.Vector2D;
+
     window.addEventListener("load", init);
 
     function init(_event: Event): void {
@@ -9,5 +11,9 @@ namespace Wingsuit {
         crc2 = canvas.getContext("2d");
         // wenn der Ursprung des Koordinatensystems in der Mitte liegen soll
         crc2.translate(canvas.width / 2, canvas.height / 2);
+        let vector: Vector2D = new Vector2D(0, 0);
+
+        console.log(vector);
     }
+
 }
