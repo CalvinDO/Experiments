b4b3f5d1c4156de2fb7fa4c6ea141b34a166644a
CalvinDO <calvindelloro@mail.de>
2019-09-30 23:41:12 +0200

    initialize vectors

--- Wingsuit/Main.ts
+++ Wingsuit/Main.ts
@@ -1,19 +1,35 @@
 namespace Wingsuit {
     let crc2: CanvasRenderingContext2D;
 
+    const timeSliceInMS: number = 1;
+
     import Vector2D = Vector.Vector2D;
 
     window.addEventListener("load", init);
 
+    //p = pull, s = speed, r= result
+
+    let pVector: Vector2D = new Vector2D(0);
+    let sVector: Vector2D = new Vector2D(1, 2);
+    let rVector: Vector2D = new Vector2D(1, 2);
+
+
+
+    let i: number = 0;
+    i = i + 1;
+
+
     function init(_event: Event): void {
-        // wenn mit Canvas gearbeitet werden soll
         let canvas: HTMLCanvasElement = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
-        // wenn der Ursprung des Koordinatensystems in der Mitte liegen soll
         crc2.translate(canvas.width / 2, canvas.height / 2);
-        let vector: Vector2D = new Vector2D(0, 0);
-
-        console.log(vector);
     }
+
+
+    function animate(): void {
+        window.setTimeout(animate, timeSliceInMS);
+
+    }
+
 
 }
