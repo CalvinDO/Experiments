771e6652c1f05e35e464d04e77f65057e726a637
CalvinDO <calvindelloro@mail.de>
2019-09-30 23:41:57 +0200

    Initialized Vectors

--- Wingsuit/Main.js
+++ Wingsuit/Main.js
@@ -1,16 +1,22 @@
 var Wingsuit;
 (function (Wingsuit) {
     let crc2;
+    const timeSliceInMS = 1;
     var Vector2D = Vector.Vector2D;
     window.addEventListener("load", init);
+    //p = pull, s = speed, r= result
+    let pVector = new Vector2D(0);
+    let sVector = new Vector2D(1, 2);
+    let rVector = new Vector2D(1, 2);
+    let i = 0;
+    i = i + 1;
     function init(_event) {
-        // wenn mit Canvas gearbeitet werden soll
         let canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
-        // wenn der Ursprung des Koordinatensystems in der Mitte liegen soll
         crc2.translate(canvas.width / 2, canvas.height / 2);
-        let vector = new Vector2D(0, 0);
-        console.log(vector);
+    }
+    function animate() {
+        window.setTimeout(animate, timeSliceInMS);
     }
 })(Wingsuit || (Wingsuit = {}));
 //# sourceMappingURL=Main.js.map
