14db8b74929b68220b3c68d4e6aa7dde74e5cc80
CalvinDO <calvindelloro@mail.de>
2019-10-02 00:45:01 +0200

    Trägheit

--- Wingsuit/Main.js
+++ Wingsuit/Main.js
@@ -5,19 +5,31 @@ var Wingsuit;
     const box = document.querySelector('.Box');
     // Initial position
     let position = 0;
+    let gravity = 5;
     var Vector2D = Vector.Vector2D;
     window.addEventListener("load", init);
-    //p = pull, s = speed, r= result
-    let pVector = new Vector2D(0, 0);
-    let sVector = new Vector2D(0, 0);
-    let rVector = new Vector2D(0, 0);
+    window.addEventListener("mousemove", trackMouseMove);
+    let vPull = new Vector2D(0, 0);
+    let vSpeed = new Vector2D(0, 0);
+    let vResult = new Vector2D(0, 0);
+    let vBall = new Vector2D(0, 0);
+    let vPointer = new Vector2D(0, 0);
+    let vGravity = new Vector2D(0, gravity);
+    let vFriction = new Vector2D(0, 0);
+    let xMouse = 0;
+    let yMouse = 0;
     let i = 0;
+    let canvas;
     function init(_event) {
-        let canvas = document.querySelector("canvas");
+        canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
         crc2.translate(canvas.width / 2, canvas.height / 2);
         animate();
-        drawBackground(canvas.width / -2, canvas.height / -2, canvas.width, canvas.height);
+    }
+    function trackMouseMove(_event) {
+        // console.log(_event.clientX, _event.clientY);
+        xMouse = _event.clientX - canvas.width / 2;
+        yMouse = _event.clientY - canvas.height / 2;
     }
     function drawBackground(_x, _y, _w, _h) {
         crc2.beginPath();
@@ -33,35 +45,46 @@ var Wingsuit;
         crc2.beginPath();
         crc2.strokeStyle = "black";
         crc2.fillStyle = "black";
-        crc2.arc(0, 0, _radius, 0 * Math.PI, 2 * Math.PI, null);
+        crc2.arc(vBall.x, vBall.y, _radius, 0 * Math.PI, 2 * Math.PI, null);
         crc2.stroke();
         crc2.fill();
     }
-    /*
-    function drawPointer(_radius: number){
+    function drawPointer(_radius) {
         crc2.beginPath();
         crc2.strokeStyle = "green";
-        crc2.arc(0, 0, _radius, 0 * Math.PI, 2 * Math.PI, null);
-        crc2.stroke( );
+        crc2.fillStyle = "green";
+        crc2.arc(xMouse, yMouse, _radius, 0 * Math.PI, 2 * Math.PI, null);
+        crc2.stroke();
         crc2.fill();
     }
-   
-    function drawPull(_startX: number, _startY: number, _endX: number, _endY: number, _colorAngle: number, _width: number): void {
-    crc2.beginPath();
-    crc2.strokeStyle = "black";
-    crc2.lineWidth = _width;
-    crc2.moveTo(_startX, _startY);
-    crc2.lineTo(_endX, _endY);
-    crc2.stroke();
-    }
- 
     function moveBall() {
+        vPointer.x = xMouse;
+        vPointer.y = yMouse;
+        vPull = vBall.getDiff(vPointer);
+        vPull.x *= -1 / 100;
+        vPull.y *= -1 / 100;
+        vSpeed.add(vGravity);
+        vSpeed.add(vPull);
+        vFriction.x = vSpeed.x / 50;
+        vFriction.y = vSpeed.y / 50;
+        vSpeed.subtract(vFriction);
+        vBall.add(vSpeed);
+        console.log(vPointer, vPull, vSpeed, vBall, vFriction);
     }
- 
-    */
+    function drawPull(_width) {
+        crc2.beginPath();
+        crc2.strokeStyle = "black";
+        crc2.lineWidth = _width;
+        crc2.moveTo(vBall.x, vBall.y);
+        crc2.lineTo(vPointer.x, vPointer.y);
+        crc2.stroke();
+    }
     function animate() {
-        i += 1;
-        console.log(i);
+        drawBackground(canvas.width / -2, canvas.height / -2, canvas.width, canvas.height);
+        drawPointer(25);
+        moveBall();
+        drawBall(50);
+        drawPull(5);
         requestAnimationFrame(animate);
     }
 })(Wingsuit || (Wingsuit = {}));
--- Wingsuit/Main.ts
+++ Wingsuit/Main.ts
@@ -5,30 +5,49 @@ namespace Wingsuit {
     const box = document.querySelector('.Box');
     // Initial position
     let position = 0;
-
+    let gravity = 5;
 
     import Vector2D = Vector.Vector2D;
 
 
     window.addEventListener("load", init);
+    window.addEventListener("mousemove", trackMouseMove);
 
-    //p = pull, s = speed, r= result
 
-    let pVector: Vector2D = new Vector2D(0, 0);
-    let sVector: Vector2D = new Vector2D(0, 0);
-    let rVector: Vector2D = new Vector2D(0, 0);
+    let vPull: Vector2D = new Vector2D(0, 0);
+    let vSpeed: Vector2D = new Vector2D(0, 0);
+    let vResult: Vector2D = new Vector2D(0, 0);
+    let vBall: Vector2D = new Vector2D(0, 0);
+    let vPointer: Vector2D = new Vector2D(0, 0);
+    let vGravity: Vector2D = new Vector2D(0, gravity);
+    let vFriction: Vector2D = new Vector2D(0, 0);
+
+
+
+    let xMouse: number = 0;
+    let yMouse: number = 0;
+
 
     let i: number = 0;
+    let canvas: HTMLCanvasElement;
+
+
 
 
     function init(_event: Event): void {
-        let canvas: HTMLCanvasElement = document.querySelector("canvas");
+        canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
         crc2.translate(canvas.width / 2, canvas.height / 2);
 
         animate();
-        drawBackground(canvas.width / -2, canvas.height / -2, canvas.width, canvas.height);
     }
+
+    function trackMouseMove(_event: MouseEvent): void {
+        // console.log(_event.clientX, _event.clientY);
+        xMouse = _event.clientX - canvas.width / 2;
+        yMouse = _event.clientY - canvas.height / 2;
+    }
+
 
     function drawBackground(_x: number, _y: number, _w: number, _h: number) {
         crc2.beginPath();
@@ -46,39 +65,64 @@ namespace Wingsuit {
         crc2.beginPath();
         crc2.strokeStyle = "black";
         crc2.fillStyle = "black";
-        crc2.arc(0, 0, _radius, 0 * Math.PI, 2 * Math.PI, null);
+        crc2.arc(vBall.x, vBall.y, _radius, 0 * Math.PI, 2 * Math.PI, null);
         crc2.stroke();
         crc2.fill()
     }
 
-    /*
-    function drawPointer(_radius: number){
+
+    function drawPointer(_radius: number) {
         crc2.beginPath();
         crc2.strokeStyle = "green";
-        crc2.arc(0, 0, _radius, 0 * Math.PI, 2 * Math.PI, null);
-        crc2.stroke( );
+        crc2.fillStyle = "green";
+        crc2.arc(xMouse, yMouse, _radius, 0 * Math.PI, 2 * Math.PI, null);
+        crc2.stroke();
         crc2.fill();
     }
-   
-    function drawPull(_startX: number, _startY: number, _endX: number, _endY: number, _colorAngle: number, _width: number): void {
-    crc2.beginPath();
-    crc2.strokeStyle = "black";
-    crc2.lineWidth = _width;
-    crc2.moveTo(_startX, _startY);
-    crc2.lineTo(_endX, _endY);
-    crc2.stroke();
-    }
- 
+
     function moveBall() {
+
+        vPointer.x = xMouse;
+        vPointer.y = yMouse;
+
+        vPull = vBall.getDiff(vPointer);
+        vPull.x *= -1 / 100;
+        vPull.y *= -1 / 100;
+
+        vSpeed.add(vGravity);
+        vSpeed.add(vPull);
+        
+        vFriction.x= vSpeed.x/50;
+        vFriction.y= vSpeed.y/50;
+        
+        vSpeed.subtract(vFriction);
+
+        vBall.add(vSpeed);
+
+        console.log(vPointer, vPull, vSpeed, vBall, vFriction);
     }
- 
-    */
+
+
+
+
+    function drawPull(_width: number): void {
+        crc2.beginPath();
+        crc2.strokeStyle = "black";
+        crc2.lineWidth = _width;
+        crc2.moveTo(vBall.x, vBall.y);
+        crc2.lineTo(vPointer.x, vPointer.y);
+        crc2.stroke();
+    }
+
 
 
     function animate() {
 
-        i += 1;
-        console.log(i);
+        drawBackground(canvas.width / -2, canvas.height / -2, canvas.width, canvas.height);
+        drawPointer(25);
+        moveBall();
+        drawBall(50);
+        drawPull(5);
 
         requestAnimationFrame(animate);
     }
