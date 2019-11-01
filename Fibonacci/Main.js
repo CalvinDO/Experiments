9ecfa4db0a9a13883aeb2edefc90a09ece4e244d
CalvinDO <calvindelloro@mail.de>
2019-11-01 21:22:07 +0100

    implemented fibonacci calculation

--- Fibonacci/Main.js
+++ Fibonacci/Main.js
@@ -3,12 +3,19 @@ var Fibonacci;
     let crc2;
     let canvas;
     window.addEventListener("load", init);
+    var Vector2D = Vector.Vector2D;
+    let fibonacciArray = new Array();
+    let vAccelaration = new Vector2D(0, 0);
+    let vSpeed = new Vector2D(0, 0);
+    let vBall = new Vector2D(0, 0);
+    let maxRecursion = 30;
     function init(_event) {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
         crc2.translate(canvas.width / 2, canvas.height / 2);
         drawBackground();
-        console.log(calculateSequence(9));
+        animate();
+        createFibArray();
     }
     function drawBackground() {
         crc2.fillStyle = "black";
@@ -21,6 +28,27 @@ var Fibonacci;
         else {
             return calculateSequence(n - 1) + calculateSequence(n - 2);
         }
+    }
+    function createFibArray() {
+        for (let i = 0; i < maxRecursion; i++) {
+            fibonacciArray[i] = calculateSequence(i);
+        }
+        console.log(fibonacciArray);
+    }
+    function moveBall() {
+    }
+    function drawBall(fibDigit) {
+        crc2.beginPath();
+        crc2.strokeStyle = "black";
+        crc2.lineWidth = 2;
+        crc2.moveTo(vBall.x, vBall.y);
+        crc2.lineTo(0, 0);
+        crc2.stroke();
+    }
+    function animate() {
+        moveBall();
+        //drawBall();
+        requestAnimationFrame(animate);
     }
 })(Fibonacci || (Fibonacci = {}));
 //# sourceMappingURL=Main.js.map
--- Fibonacci/Main.ts
+++ Fibonacci/Main.ts
@@ -3,12 +3,24 @@ namespace Fibonacci {
     let canvas: HTMLCanvasElement;
     window.addEventListener("load", init);
 
+
+    import Vector2D = Vector.Vector2D;
+
+    let fibonacciArray = new Array();
+    let vAccelaration: Vector2D = new Vector2D(0, 0);
+    let vSpeed: Vector2D = new Vector2D(0, 0);
+    let vBall: Vector2D = new Vector2D(0, 0);
+    let maxRecursion: number = 30;
+
+
+
     function init(_event: Event): void {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
         crc2.translate(canvas.width / 2, canvas.height / 2);
         drawBackground();
-        console.log(calculateSequence(9));
+        animate();
+        createFibArray();
     }
 
     function drawBackground(): void {
@@ -22,5 +34,34 @@ namespace Fibonacci {
         } else {
             return calculateSequence(n - 1) + calculateSequence(n - 2);
         }
+    }
+
+    function createFibArray() {
+        for (let i = 0; i < maxRecursion; i++) {
+            fibonacciArray[i] = calculateSequence(i);
+        }
+        console.log(fibonacciArray);
+    }
+
+
+
+    function moveBall() {
+
+    }
+
+    function drawBall(fibDigit: number) {
+        crc2.beginPath();
+        crc2.strokeStyle = "black";
+        crc2.lineWidth = 2;
+        crc2.moveTo(vBall.x, vBall.y);
+        crc2.lineTo(0, 0);
+        crc2.stroke();
+    }
+
+    function animate() {
+        moveBall();
+        //drawBall();
+
+        requestAnimationFrame(animate);
     }
 }
