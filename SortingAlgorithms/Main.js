22adf3e5933ca91d25c6fdf9102507dbb6b1f846
CalvinDO <calvindelloro@mail.de>
2020-07-30 21:23:13 +0200

    insertion sort

--- SortingAlgorithms/Main.js
+++ SortingAlgorithms/Main.js
@@ -1,21 +1,26 @@
 var SortingAlgorithms;
 (function (SortingAlgorithms) {
     window.addEventListener("load", init);
-    let amount = 20;
+    let amount = 360;
     let randomFactor = 1;
     let unsorted;
-    let currentArray;
     let accesses = 0;
     let sortingComplete;
     let canvas;
     let crc2;
     let currentFrame = 0;
     let sortIndex = 0;
+    let insertSubtractIndex = 1;
+    let initialCompare;
+    let radius = 400;
+    let speed = 1;
+    let lastFirstIndex;
+    let lastSecondIndex;
     function init(_event) {
         selectTags();
         unsorted = [];
         for (let index = 0; index < amount; index++) {
-            let randomNumber = +(amount * randomFactor * Math.random()).toFixed(0);
+            let randomNumber = (amount * randomFactor * Math.random());
             unsorted.push(randomNumber);
         }
         animate();
@@ -23,14 +28,7 @@ var SortingAlgorithms;
     function selectTags() {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
-    }
-    function bubbleSortSingle(_index) {
-        if (unsorted[_index] > unsorted[_index + 1]) {
-            let temp = unsorted[_index + 1];
-            unsorted[_index + 1] = unsorted[_index];
-            unsorted[sortIndex] = temp;
-            accesses++;
-        }
+        crc2.translate(canvas.width / 2, canvas.height / 2);
     }
     function isSorted(_input) {
         for (let checkIndex = 0; checkIndex < _input.length; checkIndex++) {
@@ -40,8 +38,21 @@ var SortingAlgorithms;
         }
         return true;
     }
+    function swap(_firstIndex, _secondIndex, _inputArray) {
+        let temp = _inputArray[_firstIndex];
+        _inputArray[_firstIndex] = _inputArray[_secondIndex];
+        _inputArray[_secondIndex] = temp;
+        accesses++;
+        lastFirstIndex = _firstIndex;
+        lastSecondIndex = _secondIndex;
+    }
+    function bubbleSortSingle(_index) {
+        if (unsorted[_index] > unsorted[_index + 1]) {
+            swap(_index, _index + 1, unsorted);
+        }
+    }
     function animateBubbleSort() {
-        sortIndex++;
+        sortIndex += 1;
         if (sortIndex >= unsorted.length) {
             sortIndex = 0;
         }
@@ -49,24 +60,115 @@ var SortingAlgorithms;
         if (isSorted(unsorted)) {
             sortingComplete = true;
         }
-        console.log(arrayToString(unsorted));
+    }
+    function animateImprovedBubbleSort() {
+        sortIndex += speed;
+        if (sortIndex >= unsorted.length || sortIndex <= 0) {
+            speed *= -1;
+        }
+        bubbleSortSingle(sortIndex);
+        if (isSorted(unsorted)) {
+            sortingComplete = true;
+        }
+    }
+    function insertionSortSingle() {
+        let insertIndex = sortIndex - insertSubtractIndex;
+        if (insertIndex < 0) {
+            insertIndex = 0;
+        }
+        if (initialCompare < unsorted[insertIndex]) {
+            swap(insertIndex, insertIndex + 1, unsorted);
+            insertSubtractIndex++;
+        }
+        else {
+            sortIndex++;
+            initialCompare = unsorted[sortIndex];
+            insertSubtractIndex = 1;
+        }
+    }
+    function animateInsertionSort() {
+        if (sortIndex == 0) {
+            initialCompare = unsorted[sortIndex];
+        }
+        insertionSortSingle();
+        if (isSorted(unsorted)) {
+            sortingComplete = true;
+        }
+    }
+    function drawCurrentArray(_values) {
+        for (let index = 0; index < _values.length; index++) {
+            let currentRadiant = index * (Math.PI / 180);
+            let currentAngle = _values[index];
+            let newX = radius * Math.sin(currentRadiant);
+            let newY = radius * Math.cos(currentRadiant);
+            crc2.beginPath();
+            crc2.lineWidth = 2;
+            crc2.strokeStyle = "hsl(" + currentAngle + ",100%, 50%, 1)";
+            crc2.moveTo(0, 0);
+            crc2.lineTo(newX, newY);
+            crc2.stroke();
+        }
+    }
+    function drawPointer() {
+        let currentRadiant = sortIndex * (Math.PI / 180);
+        let newX = radius * Math.sin(currentRadiant);
+        let newY = radius * Math.cos(currentRadiant);
+        crc2.beginPath();
+        crc2.lineWidth = 5;
+        crc2.strokeStyle = "red";
+        crc2.moveTo(0, 0);
+        crc2.lineTo(newX, newY);
+        crc2.stroke();
+    }
+    function drawSwitch(_first, _second) {
+        let currentRadiant = _first * (Math.PI / 180);
+        let newX = radius * Math.sin(currentRadiant);
+        let newY = radius * Math.cos(currentRadiant);
+        let currentAngle = unsorted[_first];
+        crc2.beginPath();
+        crc2.lineWidth = 3;
+        crc2.strokeStyle = "hsl(" + currentAngle + ",100%, 50%, 1)";
+        crc2.moveTo(0, 0);
+        crc2.lineTo(newX, newY);
+        crc2.stroke();
+        let currentRadiant2 = _second * (Math.PI / 180);
+        let newX2 = radius * Math.sin(currentRadiant2);
+        let newY2 = radius * Math.cos(currentRadiant2);
+        crc2.beginPath();
+        crc2.lineWidth = 3;
+        crc2.strokeStyle = "hsl(" + currentAngle + ",100%, 50%, 1)";
+        "hsl(" + currentAngle + ",100%, 80%, 1)";
+        crc2.moveTo(0, 0);
+        crc2.lineTo(newX2, newY2);
+        crc2.stroke();
     }
     function arrayToString(_input) {
         let output = "";
         for (let index = 0; index < _input.length; index++) {
-            output += _input[index] + "; ";
+            let extraString = (index == sortIndex ? "(  " : "");
+            let extraString2 = (index == sortIndex ? " ) " : "");
+            output += extraString + _input[index] + " " + extraString2;
         }
         return output;
     }
     function drawBackground() {
-        crc2.fillStyle = "darkred";
+        crc2.fillStyle = "black";
         crc2.fillRect(-crc2.canvas.width, -crc2.canvas.height, crc2.canvas.width * 2, crc2.canvas.height * 2);
     }
     function animate() {
         currentFrame++;
-        drawBackground();
-        if (!sortingComplete && currentFrame % 2 == 0) {
-            animateBubbleSort();
+        if (!sortingComplete) {
+            for (let index = 0; index < 100; index++) {
+                animateInsertionSort();
+            }
+            drawBackground();
+            drawCurrentArray(unsorted);
+            drawPointer();
+            drawSwitch(lastFirstIndex, lastSecondIndex);
+        }
+        else {
+            alert(accesses);
+            alert(currentFrame);
         }
         requestAnimationFrame(animate);
     }
--- SortingAlgorithms/Main.ts
+++ SortingAlgorithms/Main.ts
@@ -1,11 +1,10 @@
 namespace SortingAlgorithms {
     window.addEventListener("load", init);
 
-    let amount: number = 20;
+    let amount: number = 360;
     let randomFactor: number = 1;
 
     let unsorted: number[];
-    let currentArray: number[];
 
     let accesses: number = 0;
     let sortingComplete: boolean;
@@ -16,13 +15,22 @@ namespace SortingAlgorithms {
     let currentFrame: number = 0;
 
     let sortIndex: number = 0;
+    let insertSubtractIndex: number = 1;
+    let initialCompare: number;
+
+    let radius: number = 400;
+
+    let speed: number = 1;
+
+    let lastFirstIndex: number;
+    let lastSecondIndex: number;
 
     function init(_event: Event): void {
         selectTags();
         unsorted = [];
 
         for (let index: number = 0; index < amount; index++) {
-            let randomNumber: number = +(amount * randomFactor * Math.random()).toFixed(0);
+            let randomNumber: number = (amount * randomFactor * Math.random());
             unsorted.push(randomNumber);
         }
         animate();
@@ -31,17 +39,10 @@ namespace SortingAlgorithms {
     function selectTags(): void {
         canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
+        crc2.translate(canvas.width / 2, canvas.height / 2);
     }
 
-    function bubbleSortSingle(_index: number) {
-        if (unsorted[_index] > unsorted[_index + 1]) {
-            let temp: number = unsorted[_index + 1];
-            unsorted[_index + 1] = unsorted[_index];
-            unsorted[sortIndex] = temp;
-            accesses++;
-        }
 
-    }
     function isSorted(_input: number[]): boolean {
         for (let checkIndex: number = 0; checkIndex < _input.length; checkIndex++) {
             if (_input[checkIndex] > _input[checkIndex + 1]) {
@@ -51,8 +52,23 @@ namespace SortingAlgorithms {
         return true;
     }
 
+    function swap(_firstIndex: number, _secondIndex: number, _inputArray: number[]): void {
+        let temp: number = _inputArray[_firstIndex];
+        _inputArray[_firstIndex] = _inputArray[_secondIndex];
+        _inputArray[_secondIndex] = temp;
+        accesses++;
+        lastFirstIndex = _firstIndex;
+        lastSecondIndex = _secondIndex;
+    }
+
+    function bubbleSortSingle(_index: number) {
+        if (unsorted[_index] > unsorted[_index + 1]) {
+            swap(_index, _index + 1, unsorted);
+        }
+    }
+
     function animateBubbleSort(): void {
-        sortIndex++;
+        sortIndex += 1;
         if (sortIndex >= unsorted.length) {
             sortIndex = 0;
         }
@@ -61,29 +77,123 @@ namespace SortingAlgorithms {
         if (isSorted(unsorted)) {
             sortingComplete = true;
         }
-        console.log(arrayToString(unsorted));
+    }
+
+    function animateImprovedBubbleSort(): void {
+        sortIndex += speed;
+        if (sortIndex >= unsorted.length || sortIndex <= 0) {
+            speed *= -1;
+        }
+        bubbleSortSingle(sortIndex);
+
+        if (isSorted(unsorted)) {
+            sortingComplete = true;
+        }
+    }
+
+    function insertionSortSingle(): void {
+        let insertIndex: number = sortIndex - insertSubtractIndex;
+        if (insertIndex < 0) {
+            insertIndex = 0;
+        }
+        if (initialCompare < unsorted[insertIndex]) {
+            swap(insertIndex, insertIndex + 1, unsorted);
+            insertSubtractIndex++;
+        } else {
+            sortIndex++;
+            initialCompare = unsorted[sortIndex];
+            insertSubtractIndex = 1;
+        }
+    }
+    function animateInsertionSort(): void {
+        if (sortIndex == 0) {
+            initialCompare = unsorted[sortIndex];
+        }
+        insertionSortSingle();
+
+        if (isSorted(unsorted)) {
+            sortingComplete = true;
+        }
+    }
+
+    function drawCurrentArray(_values: number[]): void {
+        for (let index: number = 0; index < _values.length; index++) {
+            let currentRadiant: number = index * (Math.PI / 180);
+            let currentAngle: number = _values[index];
+            let newX: number = radius * Math.sin(currentRadiant);
+            let newY: number = radius * Math.cos(currentRadiant);
+            crc2.beginPath();
+            crc2.lineWidth = 2;
+            crc2.strokeStyle = "hsl(" + currentAngle + ",100%, 50%, 1)";
+            crc2.moveTo(0, 0);
+            crc2.lineTo(newX, newY);
+            crc2.stroke();
+        }
+    }
+    function drawPointer(): void {
+        let currentRadiant: number = sortIndex * (Math.PI / 180);
+        let newX: number = radius * Math.sin(currentRadiant);
+        let newY: number = radius * Math.cos(currentRadiant);
+        crc2.beginPath();
+        crc2.lineWidth = 5;
+        crc2.strokeStyle = "red";
+        crc2.moveTo(0, 0);
+        crc2.lineTo(newX, newY);
+        crc2.stroke();
+    }
+    function drawSwitch(_first: number, _second: number): void {
+        let currentRadiant: number = _first * (Math.PI / 180);
+        let newX: number = radius * Math.sin(currentRadiant);
+        let newY: number = radius * Math.cos(currentRadiant);
+        let currentAngle: number = unsorted[_first];
+        crc2.beginPath();
+        crc2.lineWidth = 3;
+        crc2.strokeStyle = "hsl(" + currentAngle + ",100%, 50%, 1)";
+        crc2.moveTo(0, 0);
+        crc2.lineTo(newX, newY);
+        crc2.stroke();
+
+        let currentRadiant2: number = _second * (Math.PI / 180);
+        let newX2: number = radius * Math.sin(currentRadiant2);
+        let newY2: number = radius * Math.cos(currentRadiant2);
+        crc2.beginPath();
+        crc2.lineWidth = 3;
+        crc2.strokeStyle = "hsl(" + currentAngle + ",100%, 50%, 1)"; "hsl(" + currentAngle + ",100%, 80%, 1)";
+        crc2.moveTo(0, 0);
+        crc2.lineTo(newX2, newY2);
+        crc2.stroke();
     }
 
     function arrayToString(_input: number[]): string {
         let output: string = "";
 
         for (let index: number = 0; index < _input.length; index++) {
-            output += _input[index] + "; ";
+            let extraString: string = (index == sortIndex ? "(  " : "");
+            let extraString2: string = (index == sortIndex ? " ) " : "");
+            output += extraString + _input[index] + " " + extraString2;
         }
         return output;
     }
     function drawBackground(): void {
-        crc2.fillStyle = "darkred";
+        crc2.fillStyle = "black";
         crc2.fillRect(-crc2.canvas.width, -crc2.canvas.height, crc2.canvas.width * 2, crc2.canvas.height * 2);
     }
 
     function animate(): void {
         currentFrame++;
 
-        drawBackground();
+        if (!sortingComplete) {
+            for (let index: number = 0; index < 100; index++) {
+                animateInsertionSort();
+            }
+            drawBackground();
+            drawCurrentArray(unsorted);
+            drawPointer();
 
-        if (!sortingComplete && currentFrame % 2 == 0) {
-            animateBubbleSort();
+            drawSwitch(lastFirstIndex, lastSecondIndex);
+        } else {
+            alert(accesses);
+            alert(currentFrame);
         }
         requestAnimationFrame(animate);
     }
